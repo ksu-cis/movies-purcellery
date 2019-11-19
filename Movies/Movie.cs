931c9dee826ae1bfdc15b1b7935611a88d04e3c3
Braden Purcell <purcellery@ksu.edu>
2019-11-19 15:18:03 -0600

    Changes we did in class

--- Movies/Movie.cs
+++ Movies/Movie.cs
@@ -11,10 +11,23 @@ namespace Movies
 
         public string MPAA_Rating { get; set; }
 
+        public string Director { get; set; }
+
         public string Major_Genre { get; set; }
 
         public float? IMDB_Rating { get; set; }
 
         public float? Rotten_Tomatoes_Rating { get; set; }
+
+        public string Release_Date { get; set; }
+
+        public string Release_Year
+        {
+            get
+            {
+                string [] parts = Release_Date.Split(" ");
+                return parts[parts.Length - 1];
+            }
+        }
     }
 }
--- Movies/MovieDatabase.cs
+++ Movies/MovieDatabase.cs
@@ -27,5 +27,50 @@ namespace Movies
         }
 
         public List<Movie> All { get { return movies; } }
+
+        public List<Movie> Search(string term)
+        {
+            List<Movie> result = new List<Movie>();
+
+            foreach (Movie movie in movies)
+            {
+                if (movie.Title.Contains(term, StringComparison.OrdinalIgnoreCase)
+                    || (movie.Director != null && movie.Director.Contains(term, StringComparison.OrdinalIgnoreCase)))
+                {
+                    result.Add(movie);
+                }
+            }
+            return result;
+        }
+
+        public List<Movie> FilterByMPAA(List<Movie> movies, List<string> mpaa)
+        {
+            List<Movie> result = new List<Movie>();
+
+            foreach(Movie movie in movies)
+            {
+                if (mpaa.Contains(movie.MPAA_Rating))
+                {
+                    result.Add(movie);
+                }
+            }
+
+            return result;
+        }
+
+        public List<Movie> FilterByMinIMDB(List<Movie> movies, float min)
+        {
+            List<Movie> result = new List<Movie>();
+
+            foreach(Movie movie in movies)
+            {
+                if (movie.IMDB_Rating != null && movie.IMDB_Rating >= min)
+                {
+                    result.Add(movie);
+                }
+            }
+
+            return result;
+        }
     }
 }
